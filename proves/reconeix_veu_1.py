a7746b4d873d2c262c7fe4038940e4ef400a0444
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2026-03-11 07:16:01 +0100

    Add sleep delay at the end of the script
    Added a sleep delay at the end of the file.

--- proves/reconeix_veu_1.py
+++ proves/reconeix_veu_1.py
@@ -8,7 +8,7 @@ import music
 
 i2c.init(freq=100000)    # inicialització de l'I2C
 sensor = VeuDF2301Q()    # inicialització mòdul veu (DF2301Q)
-sensor.configurar (volum=7, wake_time=20, mute=False)  # wake_time: temps despert
+sensor.configurar (volum=7, wake_time=20, mute=False)  # wake_time: temps despert (segons)
 
 mode ='A'
 
@@ -49,4 +49,5 @@ def reconeix_veu():
     elif cmd == 8:    
         xgo.executar_accio (18)     
        
+
     sleep(500)
