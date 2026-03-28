f42b69cf605bc19ab00711aa2d3acb83c63a0d95
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2026-03-28 07:24:22 +0100

    Refactor action functions and update comments

--- ESP32/microbit_link.py
+++ ESP32/microbit_link.py
@@ -3,23 +3,23 @@
 from machine import Pin
 from time import sleep_ms
 
-_pin_out = None    # permet definir el pin de comunicació. per defecte: 16)
+_pin_out = None    # permet definir el pin de comunicació. en el meu cas: 16)
 
 # durada del pols (ms) segon l'ordre a transmetre (personalitzar els noms):
 _ORDRES = {
-    "ACCIO_1": 60,
-    "ACCIO_2": 100,
-    "ACCIO_3": 140,
-    "ACCIO_4": 180,
-    "ACCIO_5": 220,
-    "ACCIO_6": 260,
-    "ACCIO_7": 300,
-    "ACCIO_8": 340,
-    "ACCIO_9": 380,
-    "ACCIO_10": 420,
+    "A": 60,
+    "B": 100,
+    "C": 140,
+    "D": 180,
+    "E": 220,
+    "F": 260,
+    "G": 300,
+    "H": 340,
+    "I": 380,
+    "J": 420,
 }
 
-def init(pin_num=16):    # permet definir al main el pin de comunicació. per defecte: 16
+def init(pin_num=16):    # permet definir al main el pin de comunicació. en el meu cas: 16
     global _pin_out
     _pin_out = Pin(pin_num, Pin.OUT)
     _pin_out.value(0)
@@ -39,23 +39,23 @@ def envia_ordre(nom):    # funció genèrica d'enviament del pols
 
 # ==== funcions específiques d'enviament ====
 
-def accio_1():
-    envia_ordre("ACCIO_1")
-def accio_2():
-    envia_ordre("ACCIO_2")
-def accio_3():
-    envia_ordre("ACCIO_3")
-def accio_4():
-    envia_ordre("ACCIO_4")
-def accio_5():
-    envia_ordre("ACCIO_5")
-def accio_6():
-    envia_ordre("ACCIO_6")
-def accio_7():
-    envia_ordre("ACCIO_7")
-def accio_8():
-    envia_ordre("ACCIO_8")
-def accio_9():
-    envia_ordre("ACCIO_9")
-def accio_10():
-    envia_ordre("ACCIO_10")
+def a():
+    envia_ordre("A")
+def b():
+    envia_ordre("B")
+def c():
+    envia_ordre("C")
+def d():
+    envia_ordre("D")
+def e():
+    envia_ordre("E")
+def f():
+    envia_ordre("F")
+def g():
+    envia_ordre("G")
+def h():
+    envia_ordre("H")
+def i():
+    envia_ordre("I")
+def j():
+    envia_ordre("J")
