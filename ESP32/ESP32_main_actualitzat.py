73a3115b11dbf372dc3f07f90af3f81baaac93e6
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2026-05-17 11:50:17 +0200

    17-05-26

--- ESP32/ESP32_main_actualitzat.py
+++ ESP32/ESP32_main_actualitzat.py
@@ -3,20 +3,26 @@ from machine import Pin, TouchPad, I2C
 from time import sleep_ms
 import microbit_link as mb
 import veu
+import husky
 
 mb.init(16)
 led = Pin(15, Pin.OUT)    # led integrat
-tBreak = TouchPad(Pin(3))
+#tBreak = TouchPad(Pin(3))
 tActiva = TouchPad(Pin(2))
 
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=100000)  # inicialitza I2C pels pins 9 (SCL), 8 (SDA)
 
-# inicialitza i configura el sensor:
+# inicialitza i configura el sensor de veu:
 sensor = veu.VeuDF2301Q(i2c)
 sensor.configurar(volum=7, wake_time=20, mute=False)
 
-# per comprovar si es reconeix el sensor (si cal):
-print("scan:", i2c.scan())  # hauria de retornar [100]
+# inicialitza i configura la càmera:
+cam = husky.HuskyLens(i2c)
+cam.set_algorithm(husky.OBJECT)  # mode reconeixement d’objectes
+
+# per comprovar si l'I2C reconeix els sensors:
+print("scan:", i2c.scan())     # hauria de retornar [100,50] 100: veu / 50 : HuskyLens
+print ("husky:", cam.knock())  # prova de comunicació amb la càmera. Si respon correctament per I2C retorna True
 
 sleep_ms(1000)  # pausa d'1 s per estabilitzar (imprescindible per als touch)
 
@@ -56,17 +62,20 @@ def reconeix_veu():
         mb.b()    # pols de durada C
         print("STOP")
 
+def mira():
+    b = cam.get_block()    # mira
+    if b:    # si veu alguna cosa ...
+        print (b["id"])    # retorna l’id del que veu (0 si no reconeix)
+
 # ==============================================
 
 while True:
-    if tBreak.read() > 15000:
-        print("break")
-        break
 
     if tActiva.read() > 15000:
         mb.k()
         print("activat")
 
-    reconeix_veu()
+    # reconeix_veu()
+    mira()
   
-    sleep_ms(50)
+    sleep_ms(10)
